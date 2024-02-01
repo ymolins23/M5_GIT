1f4980892d345235a77193050cbbd74ab830833a
ymolins23 <ymolins23@ilg.cat>
2024-02-01 17:33:14 +0100

    Cambios en main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const gcd = (a, b) => (!b ? a : gcd(b, a % b));
 const gcdMultiple = (...arr) => [...arr].reduce((a, b) => gcd(a, b));
 
-gcdMultiple(8, 3); // 3
-gcdMultiple(...[12, 8, 3]); // 3
+gcdMultiple(8, 3);
+gcdMultiple(...[12, 8, 3]); //3
 
